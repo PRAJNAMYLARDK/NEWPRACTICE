240276e00514348aa9dd0b323938cfbe2419d341
Admin <Admin@DESKTOP-FCOVEUD>
2022-06-01 10:01:19 +0530

    this is an second test2

---
+++ src/main/java/githubpractice2/ClassTest2.java
@@ -0,0 +1,9 @@
+package githubpractice2;
+
+public class ClassTest2 {
+	public static void main(String[] args) {
+		//comment this is an second test
+		System.out.println("this is an second test");
+	}
+
+}
